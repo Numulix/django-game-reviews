38cc82ba262c87e800e516add718f99f85b18349
Jovan Babic <jbabic999@gmail.com>
2020-12-24 23:18:51 +0100

    Added a basic template and tested DB connection

--- gamereviews/main/views.py
+++ gamereviews/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Executed statement!')
+    allGames = Game.objects.all()
+    return render(request, 'main/index.html', { 'games': allGames })
