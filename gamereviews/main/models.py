32f7fc840c2c8876ef860854bb0ae340dc198864
Jovan Babic <jbabic999@gmail.com>
2020-12-24 22:52:54 +0100

    Created Game model

--- gamereviews/main/models.py
+++ gamereviews/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=50)
+    release_year = models.IntegerField(validators=[MinValueValidator(1950), MaxValueValidator(2020)])
+    company = models.CharField(max_length=20)
+    description = models.TextField(max_length=512)
+    avg_rating = models.FloatField()
+
+    def __str__(self):
+        return self.name
